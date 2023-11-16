9b9bf765515d2763bfcb02597271b289c457dde2
Sandro Ferreira de Brito <sandro.brito@fleetcor.com.br>
2023-11-16 19:14:52 -0300

    Project created

---
+++ src/main/java/com/santest/backendjavalistadejogos/BackendJavaListadejogosApplication.java
@@ -0,0 +1,13 @@
+package com.santest.backendjavalistadejogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaListadejogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJavaListadejogosApplication.class, args);
+	}
+
+}
