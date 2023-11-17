cdf72679eaa0662f16f307e1079481eb5da980e0
Sandro Ferreira de Brito <sandro.brito@fleetcor.com.br>
2023-11-17 10:59:50 -0300

    Get game By id

--- src/main/java/com/santest/backendjavalistadejogos/controllers/GameController.java
+++ src/main/java/com/santest/backendjavalistadejogos/controllers/GameController.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.santest.backendjavalistadejogos.dto.GameDTO;
 import com.santest.backendjavalistadejogos.dto.GameMinDTO;
-import com.santest.backendjavalistadejogos.entities.Game;
 import com.santest.backendjavalistadejogos.services.GameService;
 
 @RestController
@@ -18,10 +19,18 @@ public class GameController {
 	@Autowired
 	private GameService gameService;
 	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
+	
 	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
 		List<GameMinDTO> result = gameService.findAll();
 		return result;
 	}
+	
+
 }
